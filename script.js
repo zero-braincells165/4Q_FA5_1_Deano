787ed34b46f3efa62b04ca04d8677f239e906f02
YuriLivia <143151116+zero-braincells165@users.noreply.github.com>
2024-05-13 09:26:07 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-function generateTriangle() {
+function generateShape() {
   var number = document.getElementById("numberInput").value;
-  var triangleDiv = document.getElementById("triangle");
-  triangleDiv.innerHTML = "";
+  var shapeDiv = document.getElementById("shape");
+  shapeDiv.innerHTML = "";
 
-  for (var i = number; i >= 1; i--) {
-    var line = "";
-    for (var j = number; j >= i; j--) {
-      line += i + " ";
+  if (number % 2 === 0) {
+    // Generate Triangle
+    for (var i = number; i >= 1; i--) {
+      var line = "";
+      for (var j = number; j >= i; j--) {
+        line += i + " ";
+      }
+      shapeDiv.innerHTML += line + "<br>";
     }
-    triangleDiv.innerHTML += line + "<br>";
+  } else {
+    // Generate Square
+    for (var i = number; i >= 1; i--) {
+      var line = "";
+      for (var j = number; j >= 1; j--) {
+        line += i + " ";
+      }
+      shapeDiv.innerHTML += line + "<br>";
+    }
   }
 }
