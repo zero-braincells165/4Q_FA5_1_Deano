b5b57b5848b18b49b10adc292e51a159fccd72f0
YuriLivia <143151116+zero-braincells165@users.noreply.github.com>
2024-05-13 09:19:41 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,13 @@
-function isNumeric(value) {
-  return /^-?\d+$/.test(value);
-}
+function generateTriangle() {
+  var number = document.getElementById("numberInput").value;
+  var triangleDiv = document.getElementById("triangle");
+  triangleDiv.innerHTML = "";
 
-function generateShape() {
-  var input = document.getElementById("numberInput").value;
-  var shapeOutput = document.getElementById("shapeOutput");
-  
-  // Check if the input is numerical
-  if (!isNumeric(input)) {
-    alert("Please enter a valid number.");
-    return;
-  }
-  
-  // Convert input to a number
-  var num = parseInt(input);
-  
-  // Generate shape based on the number
-  if (num % 2 === 0) {
-    var triangle = "";
-    for (var i = 1; i <= num; i++) {
-      triangle += "* ".repeat(i) + "<br>";
+  for (var i = number; i >= 1; i--) {
+    var line = "";
+    for (var j = number; j >= i; j--) {
+      line += i + " ";
     }
-    shapeOutput.innerHTML = triangle;
-  } else {
-    var rectangle = "";
-    for (var i = 0; i < num; i++) {
-      for (var j = 0; j < num; j++) {
-        rectangle += "* ";
-      }
-      rectangle += "<br>";
-    }
-    shapeOutput.innerHTML = rectangle;
+    triangleDiv.innerHTML += line + "<br>";
   }
 }
